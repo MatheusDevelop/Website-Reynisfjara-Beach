eda844656cc46882e3502663678cf72ef670ba4e
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-05-19 19:10:20 -0300

    Website finalizado

---
+++ precache-manifest.0f2b5a7ce39b91f97000136982edff9e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f19d499698a8bcdccaf5cfc503fdf3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a81958c663c8c860415f",
+    "url": "/static/css/main.d689d01a.chunk.css"
+  },
+  {
+    "revision": "371c4ff18e0528e7bbe5",
+    "url": "/static/js/2.28f51236.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.28f51236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a81958c663c8c860415f",
+    "url": "/static/js/main.d903f3f7.chunk.js"
+  },
+  {
+    "revision": "36b6d519b0bbb9e639fc",
+    "url": "/static/js/runtime-main.364905a6.js"
+  },
+  {
+    "revision": "342be37d57c857418bd5cebfe210f678",
+    "url": "/static/media/1.342be37d.jpg"
+  },
+  {
+    "revision": "1042146564eea79fabb238114921694c",
+    "url": "/static/media/2.10421465.jpg"
+  },
+  {
+    "revision": "0cae13091137475b1a7c179a46de4934",
+    "url": "/static/media/3.0cae1309.jpg"
+  },
+  {
+    "revision": "b846247d41854fe771ae325ba54f9ac4",
+    "url": "/static/media/4.b846247d.jpg"
+  },
+  {
+    "revision": "2ad7dcf011e2e3820af2877b6b614725",
+    "url": "/static/media/bimg.2ad7dcf0.png"
+  },
+  {
+    "revision": "e4cbf7923685cd73bf82ceca873d5215",
+    "url": "/static/media/formimg.e4cbf792.jpg"
+  },
+  {
+    "revision": "caef38f681e8ab6f8380514214d91162",
+    "url": "/static/media/hotel1.caef38f6.jpg"
+  },
+  {
+    "revision": "8d8dea0efdb6cb4164e712ef914c2e04",
+    "url": "/static/media/hotel2.8d8dea0e.jpg"
+  },
+  {
+    "revision": "c9d7d672473608a85a6a9bdbf8ac705a",
+    "url": "/static/media/hotel3.c9d7d672.jpg"
+  },
+  {
+    "revision": "33918f1693c97aae1c6c2cd6bf009bb0",
+    "url": "/static/media/im1.33918f16.jpg"
+  },
+  {
+    "revision": "b64815906708e2d19008602747489c94",
+    "url": "/static/media/im2.b6481590.jpg"
+  },
+  {
+    "revision": "96100eec9892f5b10a1091d09bf879ca",
+    "url": "/static/media/im3.96100eec.jpg"
+  },
+  {
+    "revision": "71b94feb143e56c04494e57a4c8eb225",
+    "url": "/static/media/im4.71b94feb.jpg"
+  },
+  {
+    "revision": "e74677a736389333738ca2e4ae3fa0d7",
+    "url": "/static/media/stars.e74677a7.svg"
+  },
+  {
+    "revision": "af254a72b2ac3fd97c3a6bdbe8218bd5",
+    "url": "/static/media/welcome.af254a72.png"
+  }
+]);
